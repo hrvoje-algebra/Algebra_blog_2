58cc0637389db84d40fcc842076e5564016f39b2
Hrvoje Antic <hrvoje.antic@gmail.com>
2018-01-12 22:09:59 +0100

    Implement autentification system

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {	
+		//Dodano zbog greske SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long
+        Schema::defaultStringLength(191);
     }
 
     /**
